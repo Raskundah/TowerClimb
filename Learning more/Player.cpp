bd0eed8ee674829866cb61875f9c4cbe13921d73
Raskundah <100294321+Raskundah@users.noreply.github.com>
2023-03-14 17:04:19 +0000

    added more verlet implimentation.

--- Learning more/Player.cpp
+++ Learning more/Player.cpp
@@ -3,12 +3,16 @@
 
 enum class PhysicsType {
 	FORWARD_EULER,
-	BACKWARD_EULER
+	BACKWARD_EULER,
+	SYMPLECTIC_EULER,
+	INCORRECT_VERLET,
+	VELOCITY_VERLET
 };
 
 Player::Player()
 	: SpriteObject()
 	, m_position(10,10)
+	,m_twoFramesOldPos(m_position)
 	, m_velocity()
 	, m_acceleration(100,100)
 {
@@ -18,7 +22,7 @@ Player::Player()
 
 void Player::Update(sf::Time _frameTime)
 {
-	const PhysicsType physics = PhysicsType::BACKWARD_EULER;
+	const PhysicsType physics = PhysicsType::SYMPLECTIC_EULER;
 
 	switch (physics)
 	{
@@ -28,7 +32,7 @@ void Player::Update(sf::Time _frameTime)
 
 			m_velocity = m_velocity + m_acceleration * _frameTime.asSeconds();
 
-			m_velocity *= 0.99f;
+			m_velocity *= 0.5f;
 
 
 			UpdateAcceleration();
@@ -41,13 +45,66 @@ void Player::Update(sf::Time _frameTime)
 
 		m_velocity = m_velocity + m_acceleration * _frameTime.asSeconds();
 
-		m_velocity *= 0.99f;
+		m_velocity *= 0.5f;
 
 		m_position = m_position + m_velocity * _frameTime.asSeconds();
 
 	}
 	break;
 
+	case PhysicsType::SYMPLECTIC_EULER:
+	{
+		m_velocity = m_velocity + m_acceleration * _frameTime.asSeconds();
+
+		m_velocity *= 0.5f;
+
+
+		m_position = m_position + m_velocity * _frameTime.asSeconds();
+
+
+		UpdateAcceleration();
+
+	}
+	break;
+
+	case PhysicsType::INCORRECT_VERLET: //position verlet
+	{
+		UpdateAcceleration();
+
+		sf::Vector2f lastFramePos = m_position;
+
+		//current frame pos
+		m_position = 2.0f * lastFramePos - m_twoFramesOldPos + m_acceleration * _frameTime.asSeconds() * _frameTime.asSeconds();
+
+		//two frames ago (for next frame)
+
+		m_twoFramesOldPos = lastFramePos;
+	}
+	break;
+
+	case PhysicsType::VELOCITY_VERLET:
+	{
+
+		//get half frame velocity using
+
+		//previous frame's acceleration 
+		sf::Vector2f halfFrameVel = m_velocity + m_acceleration * _frameTime.asSeconds() / 2.0f;
+
+		//get new frame position using hjalf frame velocity
+		m_position = m_position + halfFrameVel * _frameTime.asSeconds();
+
+
+		//update acceleration
+		UpdateAcceleration();
+
+
+		//get new frames velocity using half frame velocity and updated acceleration
+		m_velocity = halfFrameVel + m_acceleration * _frameTime.asSeconds();
+
+		m_velocity *= 0.5f;
+
+	}
+	break;
 	default:
 		break;
 	}
@@ -61,7 +118,7 @@ void Player::UpdateAcceleration()
 
 	m_acceleration.x = 0;
 	m_acceleration.y = 0;
-	const float ACCEL = 1000;
+	const float ACCEL = 20000;
 
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
